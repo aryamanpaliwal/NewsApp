b139661782f7a4c9d91ac3741a7a55444aafcc7c
Aryaman Paliwal <83011117+aryamanpaliwal@users.noreply.github.com>
2023-04-04 19:54:00 +0530

    This is temp commit

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState, createRef } from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
+import useStyles from "./styles.js"
+import classNames from 'classnames';
+
+const NewsCard = ({ article:{ description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
+    const classes = useStyles();
+
+    const [elRefs, setElRefs] = useState([]);
+
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
+    return (
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+            <CardActionArea href={ url } target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || "https://t4.ftcdn.net/jpg/03/29/24/37/360_F_329243708_oXkPklGDS1BMjtwrWrxepInCGbjag0DL.jpg"} />
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" components="h2">{ (new Date(publishedAt)).toDateString() }</Typography>
+                    <Typography variant="body2" color="textSecondary" components="h2">{ source.name }</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant="h5">{ title }</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" components="p">{ description }</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{ i+1 }</Typography>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default NewsCard;
